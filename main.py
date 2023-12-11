b2a9610604c566ecc258a900e0972cdf7c66b107
Flaviano Rodrigues dos Santos <flavianorodriguessantos@gmail.com>
2023-12-11 11:43:09 -0300

    Atualizando Driver a ser usado

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import selenium.webdriver
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+navegador = webdriver.Firefox()
+
+navegador.get("https://web.whatsapp.com")
